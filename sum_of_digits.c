f9a1434e6ca4379e9e3256f21e189dae871f0db9
Tanmay-Mohan22 <78509220+Tanmay-Mohan22@users.noreply.github.com>
2021-02-12 22:12:47 +0530

    created sum_of_digits.c

---
+++ sum_of_digits.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int sum_of_digit(int n)
+{
+	if(n==0)
+	{
+	return 0;
+    }
+	else
+	{
+	return(n%10+sum_of_digit(n/10));
+    } 
+}
+int main()
+{
+	int num;
+	printf("\nInput number:");
+	scanf("%d",&num);
+	int result=sum_of_digit(num);
+	printf("\nSum of digits in %d:%d\n",num,result);
+	return 0;
+}
